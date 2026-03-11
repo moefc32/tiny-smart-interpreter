38d947c5138e140f91e64b540aa0058491205475
Faizal Chan. <moefc32@gmail.com>
2026-03-11 22:34:06 +0700

    wrap hooks with try...catch

--- src/routes/hooks.server.js
+++ src/routes/hooks.server.js
@@ -4,17 +4,28 @@ import token from '$lib/server/token';
 export const handle = async ({ event, resolve }) => {
     const { cookies, url } = event;
 
-    const lang = cookies.get('lang');
-    const validLang = lang && ['en', 'id'].includes(lang);
+    try {
+        const lang = cookies.get('lang');
+        const validLang = lang && ['en', 'id'].includes(lang);
 
-    if (!validLang) {
-        cookies.set('lang', 'en', {
-            path: '/',
-            httpOnly: true,
-        });
+        if (!validLang) {
+            cookies.set('lang', 'en', {
+                path: '/',
+                httpOnly: true,
+            });
+        }
+
+        event.locals.lang = validLang ? lang : 'en';
+
+        return resolve(event);
+    } catch (e) {
+        if (e.status && e.status >= 300 && e.status < 400) {
+            throw e;
+        }
+
+        console.error('\n--- CRITICAL HOOK ERROR ---\n');
+        console.error(e);
+
+        return await resolve(event);
     }
-
-    event.locals.lang = validLang ? lang : 'en';
-
-    return resolve(event);
 };
