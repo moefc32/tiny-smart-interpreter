3f277b3d76a40e8a0201f18d7c65eb1309e92a9a
Faizal Chan. <moefc32@gmail.com>
2026-03-15 23:35:45 +0700

    adjust db schema

--- src/lib/server/db/schema.js
+++ src/lib/server/db/schema.js
@@ -2,10 +2,8 @@ import { sqliteTable, text, integer, real } from 'drizzle-orm/sqlite-core';
 import { sql } from 'drizzle-orm';
 
 export const Histories = sqliteTable('Histories', {
-	id: text('id')
-		.primaryKey()
-		.default(sql`lower(hex(randomblob(16)))`),
-
+	id: text('id').primaryKey()
+		.$defaultFn(() => sql`lower(hex(randomblob(16)))`),
 	role: text('role').notNull(),
 	text: text('text').notNull(),
 	timestamp: integer('timestamp').notNull(),
